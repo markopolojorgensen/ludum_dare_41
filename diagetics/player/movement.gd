4021a83e0cdcfb3bce2a7d4b5f669d05262fd507
Mark J <marko.polo.jorgensen@gmail.com>
2018-04-25 14:03:03 -0500

    reorganized player movement code (no functional changes)

--- diagetics/player/movement.gd
+++ diagetics/player/movement.gd
@@ -11,15 +11,45 @@ var is_down
 var enabled = true
 
 func do_movement(body, state):
-	if not enabled:
-		return
+	update_inputs()
+	enforce_max_speed(body, state)
+	natural_slowdown(body, state)
 	
+	if enabled:
+		apply_movement(body, state)
+
+func update_inputs():
 	is_left = Input.is_action_pressed("move_left")
 	is_right = Input.is_action_pressed("move_right")
 	is_up = Input.is_action_pressed("move_up")
 	is_down = Input.is_action_pressed("move_down")
-	
-	# movement
+
+func enforce_max_speed(body, state):
+	# going too fast
+	var lin_vel = state.get_linear_velocity()
+	if lin_vel.length() > max_speed:
+		# slow down
+		var slow_down_impulse = -1 * (lin_vel.length() - max_speed) * lin_vel.normalized()
+		body.apply_impulse(Vector2(), slow_down_impulse)
+
+func natural_slowdown(body, state):
+	# natural slowdown
+	var no_input_pressed = not (is_left or is_right)
+	var is_shooting = get_parent().is_shooting()
+	if no_input_pressed:
+		var lin_vel = state.get_linear_velocity()
+		var horiz_vector = Vector2(1, 0)
+		var slow_down_impulse = -1 * lin_vel.dot(horiz_vector) * horiz_vector
+		
+		if is_shooting:
+			# don't gimp kickback
+			slow_down_impulse *= 0.1
+		else:
+			slow_down_impulse *= 0.4
+		
+		body.apply_impulse(Vector2(), slow_down_impulse)
+
+func apply_movement(body, state):
 	if is_right:
 		body.apply_impulse(Vector2(), Vector2(impulse_size * state.step, 0))
 	elif is_left:
@@ -27,25 +57,6 @@ func do_movement(body, state):
 	
 	if is_down and not Input.is_action_pressed("jump"):
 		body.apply_impulse(Vector2(), Vector2(0, impulse_size * state.step))
-	
-	# going too fast
-	var lin_vel = state.get_linear_velocity()
-	if lin_vel.length() > max_speed:
-		# slow down
-		var slow_down_impulse = -1 * (lin_vel.length() - max_speed) * lin_vel.normalized()
-		body.apply_impulse(Vector2(), slow_down_impulse)
-	
-	# natural slowdown
-	var no_input_pressed = not (is_left or is_right)
-	var is_shooting = get_parent().is_shooting()
-	if no_input_pressed:
-		var horiz_vector = Vector2(1, 0)
-		var slow_down_impulse = -1 * lin_vel.dot(horiz_vector) * horiz_vector
-		if not is_shooting:
-			slow_down_impulse *= 0.4
-		else:
-			slow_down_impulse *= 0.1
-		body.apply_impulse(Vector2(), slow_down_impulse)
 
 func speed_up():
 	max_speed += 200
@@ -55,5 +66,4 @@ func slow_down():
 
 func disable():
 	enabled = false
-
 
